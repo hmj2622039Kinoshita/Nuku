f9223142b7f8aebe61b140d9b50f92b5669d8f94
hmj2622039@stu.o-hara.ac.jp <hmj2622039@stu.o-hara.ac.jp>
2026-09-07 15:10:19 +0900

    背景の仮完成

--- Nuku.cpp
+++ Nuku.cpp
@@ -2,8 +2,11 @@
 #include"Nuku.h" // ヘッダーファイル
 
 //定数定義 
-const int WIDTH = 1200, HEIGHT = 800; // ウィンドウの幅と高さのピクセル数
+const int WIDTH = 1536, HEIGHT = 896; // ウィンドウの幅と高さのピクセル数
 const int FPS = 60; // フレームレート
+
+// グローバル変数
+int imgBackGrass; // 背景画像（草）
 
 // グローバル関数
 int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
@@ -14,10 +17,16 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 	if (DxLib_Init() == -1) return -1; // ライブラリ初期化、エラーが起きたら終了
 	SetBackgroundColor(0, 0, 0); // 背景色の指定
 	SetDrawScreen(DX_SCREEN_BACK); // 描画面を裏背景にする
+	
+	initGame(); // 初期化用の関数を呼び出す
+	InitVariable(void); // ゲーム開始時の初期値
+	
 	while (1)
 	{
+		ClearDrawScreen(); // 画面をクリアにする
 
-
+		// ゲームの骨組み処理
+		scrollBG(1); // 背景のスクロール,プレイヤーの速さと連動させる
 
 
 		ScreenFlip(); // 裏画面の内容を表画面に反映させる
@@ -30,3 +39,18 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 }
 
 // 自作関数記述
+// 初期化用の関数
+void initGame(void)
+{
+	// 背景画像の読み込み
+	imgBackGrass = LoadGraph("Material/Background/Grass.png"); //背景画像（草）
+}
+
+// 背景のスクロール
+void scrollBG(int spd)
+{
+	static int backGrassX; // スクロールの位置を管理する変数
+	backGrassX = (backGrassX - spd) % WIDTH; // 背景（草）のX座標
+	DrawGraph(backGrassX, -64, imgBackGrass, false); // 背景（草）の描画
+	DrawGraph(backGrassX + 1024, -64, imgBackGrass, false);
+}
--- Nuku.h
+++ Nuku.h
@@ -1,1 +1,15 @@
 #pragma once
+
+// 構造体宣言
+struct OBJECT // プレイヤーと敵用
+{
+	float x; // x座標
+	float y; // y座標
+	float vx; // x方向の速さ
+	float speed; // 加速度
+};
+
+// 関数プロトタイプ宣言
+void initGame(void); // 初期化用の関数
+void scrollBG(int spd); // 背景のスクロール関数
+void InitVariable(void); // ゲーム開始時の初期値
