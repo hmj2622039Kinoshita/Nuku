b59f1eef4605f5951fe8448336ab5f7e5a17e7e3
hmj2622039@stu.o-hara.ac.jp <hmj2622039@stu.o-hara.ac.jp>
2026-09-08 15:09:21 +0900

    道路とプレイヤーの仮完成

--- Nuku.cpp
+++ Nuku.cpp
@@ -5,8 +5,15 @@
 const int WIDTH = 1536, HEIGHT = 896; // ウィンドウの幅と高さのピクセル数
 const int FPS = 60; // フレームレート
 
+// 構造体
+struct OBJECT player; // プレイヤーの構造体変数
+
 // グローバル変数
 int imgBackGrass; // 背景画像（草）
+int imgPlayer[2]; // プレイヤー画像
+int imgWayGrass1, imgWayGrass2; // 道路画像（草）
+int timer; // 車のアニメーションの速さ管理するためのタイマー
+
 
 // グローバル関数
 int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
@@ -18,15 +25,19 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 	SetBackgroundColor(0, 0, 0); // 背景色の指定
 	SetDrawScreen(DX_SCREEN_BACK); // 描画面を裏背景にする
 	
-	initGame(); // 初期化用の関数を呼び出す
-	InitVariable(void); // ゲーム開始時の初期値
+	InitGame(); // 初期化用の関数を呼び出す
+	InitVariable(); // ゲーム開始時の初期値
 	
 	while (1)
 	{
 		ClearDrawScreen(); // 画面をクリアにする
 
 		// ゲームの骨組み処理
-		scrollBG(1); // 背景のスクロール,プレイヤーの速さと連動させる
+		ScrollBG(1); // 背景のスクロール
+		ScrollWY(1); // 道路のスクロール
+		Player(); // プレイヤー
+
+
 
 
 		ScreenFlip(); // 裏画面の内容を表画面に反映させる
@@ -40,17 +51,70 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 
 // 自作関数記述
 // 初期化用の関数
-void initGame(void)
+void InitGame(void)
 {
-	// 背景画像の読み込み
+	// 画像の読み込み
 	imgBackGrass = LoadGraph("Material/Background/Grass.png"); //背景画像（草）
+	imgPlayer[0] = LoadGraph("Material/Robot/green1.png"); // プレイヤー画像
+	imgPlayer[1] = LoadGraph("Material/Robot/green2.png");
+	imgWayGrass1 = LoadGraph("Material/Way/grass.png"); // 道路画像（草）
+	imgWayGrass2 = LoadGraph("Material/Way/grass2.png"); 
+}
+
+// ゲーム開始時の初期値
+void InitVariable(void)
+{
+	// プレイヤーの構造体
+	player.x = WIDTH / 2 - 79; // プレイヤーの左上のX座標
+	player.y = HEIGHT - 140 - 118; // プレイヤーの左上のY座標
+	player.speed = 1; // プレイヤーのスピード（背景、道路、アニメーション、敵の車もこれで管理する）
 }
 
 // 背景のスクロール
-void scrollBG(int spd)
+void ScrollBG(int spd)
 {
 	static int backGrassX; // スクロールの位置を管理する変数
-	backGrassX = (backGrassX - spd) % WIDTH; // 背景（草）のX座標
+	backGrassX = (backGrassX - spd) % 1024; // 背景（草）のX座標
 	DrawGraph(backGrassX, -64, imgBackGrass, false); // 背景（草）の描画
 	DrawGraph(backGrassX + 1024, -64, imgBackGrass, false);
+	DrawGraph(backGrassX + 2048, -64, imgBackGrass, false);
+}
+
+// 道路のスクロール
+void ScrollWY(int spd)
+{
+	static int wayGrassX; // スクロールの位置を管理する変数
+	wayGrassX = (wayGrassX - spd) % 70; // 道路（草）のX座標
+	for (int i = 0; i < 24; i++)
+	{
+		DrawGraph(wayGrassX + i * 70, HEIGHT - 70 * 2, imgWayGrass1, true); // 道路（草）の描画
+		DrawGraph(wayGrassX + i * 70, HEIGHT - 70 , imgWayGrass2, true);
+	}
+}
+
+// プレイヤー
+void Player(void)
+{
+	if (player.speed == 0)
+	{
+		DrawGraph(player.x, player.y, imgPlayer[0], true);
+	}
+	else
+	{
+		timer++; //	アニメーションタイマー動かす
+		if (timer <= 10)
+		{
+			DrawGraph(player.x, player.y, imgPlayer[1], true);
+		}
+		else if (10 < timer && timer <= 20)
+		{
+			DrawGraph(player.x, player.y, imgPlayer[0], true);
+		}
+		else if (20 < timer)
+		{
+			DrawGraph(player.x, player.y, imgPlayer[1], true);
+			timer = 0; // タイマーリセット
+		}
+
+	}
 }
--- Nuku.h
+++ Nuku.h
@@ -5,11 +5,12 @@ struct OBJECT // プレイヤーと敵用
 {
 	float x; // x座標
 	float y; // y座標
-	float vx; // x方向の速さ
-	float speed; // 加速度
+	float speed; // 速度
 };
 
 // 関数プロトタイプ宣言
-void initGame(void); // 初期化用の関数
-void scrollBG(int spd); // 背景のスクロール関数
+void InitGame(void); // 初期化用の関数
 void InitVariable(void); // ゲーム開始時の初期値
+void ScrollBG(int spd); // 背景のスクロール関数
+void ScrollWY(int spd); // 道路のスクロール関数
+void Player(void); // プレイヤー用の関数
